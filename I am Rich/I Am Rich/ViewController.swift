913d18dc5b12a50cb11747c28d0fccdd7a285bc2
anubhavsingh16 <anubhavssingh177@gmail.com>
2019-10-19 00:50:58 +0530

    I am rich

---
+++ I am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Anubhav Singh on 15/10/19.
+//  Copyright © 2019 Anubhav Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
