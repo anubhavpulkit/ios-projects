d48bd820abd2383a747362c543bd236e10997daa
anubhavsingh16 <anubhavssingh177@gmail.com>
2020-01-06 16:15:24 +0530

    update file

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+       
+        playSound(soundname: sender.currentTitle!)
+         sender.alpha = 0.5
+        //Code should execute after 0.2 second delay.
+         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+          }
+    }
+    
+
+    func playSound(soundname: String) {
+        guard let url = Bundle.main.url(forResource: soundname, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+
+}
+
